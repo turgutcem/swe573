a68e5c9b31524abac0b4241684101045b01db665
turgutcem <turgutcemyilmazturk@gmail.com>
2022-11-07 19:19:39 +0300

    Project Setup

---
+++ swe573/src/main/java/com/swe573/swe573/InfoGathererAppApplication.java
@@ -0,0 +1,13 @@
+package com.swe573.swe573;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoGathererAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoGathererAppApplication.class, args);
+	}
+
+}
---
+++ swe573/src/test/java/com/swe573/swe573/InfoGathererAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe573.swe573;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoGathererAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

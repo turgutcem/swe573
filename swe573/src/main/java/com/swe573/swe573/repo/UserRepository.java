30ffe787146106375bc1ce436d9555a44c8671d4
turgutcem <turgutcemyilmazturk@gmail.com>
2022-12-06 05:48:05 +0300

    comment repo config

--- swe573/src/main/java/com/swe573/swe573/repo/UserRepository.java
+++ swe573/src/main/java/com/swe573/swe573/repo/UserRepository.java
@@ -2,7 +2,6 @@ package com.swe573.swe573.repo;
 
 import com.swe573.swe573.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;

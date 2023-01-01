b8202920584db695b6998800fed2f2a961f21c36
turgutcem <turgutcemyilmazturk@gmail.com>
2023-01-01 21:55:27 +0300

    Remove unused imports

--- swe573/src/main/java/com/swe573/swe573/InfoGathererAppApplication.java
+++ swe573/src/main/java/com/swe573/swe573/InfoGathererAppApplication.java
@@ -3,8 +3,6 @@ package com.swe573.swe573;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 public class InfoGathererAppApplication {
 
